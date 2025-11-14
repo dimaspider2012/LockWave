e1968f7815cd5533c44aed3408419d05431e4d30
Dima <168201985+dimaspider2012@users.noreply.github.com>
2025-11-14 11:36:25 +0200

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import fs from "fs-extra";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
